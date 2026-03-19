e8de18358ee12fc3b2bbb5a18aaad19ec6e05eee
ingrid <ingridledesma22@gmail.com>
2026-03-19 10:34:28 -0300

    actualizo tabla vehiculo agregando columna unidad y subo guia de usuario version comprimida

--- database/migrations/2025_12_18_132510_vehiculo.php
+++ database/migrations/2025_12_18_132510_vehiculo.php
@@ -12,6 +12,7 @@ return new class extends Migration
         Schema::create('vehiculos', function(Blueprint $table){
             $table->id();
             $table->string('dominio')->unique();
+            $table->string('unidad')->unique();
             $table->string('marca');
             $table->string('modelo');
             $table->integer('anio');
