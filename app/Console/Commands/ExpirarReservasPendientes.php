d4bba9fe919ccc2d9cc9c4f01c8bf40d12641c08
ingrid <ingridledesma22@gmail.com>
2026-04-20 12:16:17 -0300

    .

--- app/Console/Commands/ExpirarReservasPendientes.php
+++ app/Console/Commands/ExpirarReservasPendientes.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace App\Console\Commands;
-
 use App\Models\EstadosReserva;
 use App\Models\Reserva;
 use Carbon\Carbon;
@@ -15,7 +14,7 @@ class ExpirarReservasPendientes extends Command
      * @var string
      */
     protected $signature = 'reservas:expirar-pendientes';
-    
+
 
 
     /**
@@ -23,7 +22,7 @@ class ExpirarReservasPendientes extends Command
      *
      * @var string
      */
-    protected $description = 'Expira reservas pendientes luego de 48 horas';
+    protected $description = 'Expira reservas pendientes luego de 72 horas';
 
     /**
      * Execute the console command.
@@ -35,7 +34,7 @@ class ExpirarReservasPendientes extends Command
             //addBusinessDays le suma dos dias laborables (de L a V) a la fecha_reserva de la reserva
             //Si una reserva cae viernes, se saltea el sabado y domingo y empieza a sumar a partir del lunes (la del viernes venceria el martes o la del jueves el lunes)
             $vence = Carbon::parse($reserva->fecha_reserva)->addBusinessDays(2);
-            
+
             // Si now es mayor o igual a la fecha donde deberia vencer la reserva
             if (now()->greaterThanOrEqualTo($vence)) {
                 $reserva->update([
